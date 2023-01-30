dc78a1f230db2fe86e15a7c7d31fa5b4da4a2d4a
Steven Christe <ehsteve@users.noreply.github.com>
2023-01-30 09:22:52 -0500

    flake8 ignore

--- ccsdspy/tests/test_primary_header.py
+++ ccsdspy/tests/test_primary_header.py
@@ -47,7 +47,7 @@ def test_primary_header_contents_no_offset(cls):
     """Test if the primary header is output correctly along with the data without
     defining bit offsets"""
     num_packets = 3
-    packet = create_simple_ccsds_packet(num_packets)
+    packet = create_simple_ccsds_packet(num_packets)  # noqa: F841
 
     pkt = cls(
         [
@@ -75,7 +75,7 @@ def test_primary_header_contents_offset():
     """Test if the primary header is output correctly along with the data with
     defining bit offsets"""
     num_packets = 3
-    packet = create_simple_ccsds_packet(num_packets)
+    packet = create_simple_ccsds_packet(num_packets)  # noqa: F841
 
     pkt = FixedLength(
         [
